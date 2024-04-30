78275ac423084fe288d8e6d4fc735b271d756715
lil-j <jake@krunchy.net>
2024-04-30 00:24:34 -0700

    change formatting a bit

--- components/directory/CompanyItem.js
+++ components/directory/CompanyItem.js
@@ -4,7 +4,7 @@ export default function CompanyItem({company, isLast}) {
     return <Link
         href={`/${company.slug}`}
         className={`py-6 px-3 w-full ${!isLast ? "border-b" : ""} border-gray-300 flex gap-3 items-center`}>
-        <img alt={`${company.name} logo`} src={company.logo} className="w-16 h-16 bg-gray-200 flex-grow rounded-full"/>
+        <img alt={`${company.name} logo`} src={company.logo} className="w-16 h-16 bg-gray-200 rounded-full"/>
         <div className="flex flex-col gap-1">
             <div className="flex items-center gap-3">
                 <h1 className="font-semibold">{company.name}</h1>
